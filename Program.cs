279d60d9a05af34dffa9fd3a3c089cd72a0c1d1f
unknown <brenomax02@gmail.com>
2020-06-26 10:29:53 -0300

    Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace Aula26Interfaces
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Produto prod = new Produto();
+           Produto prod1 = new Produto(1 , "Livro", 24.99f);
+           Produto prod2 = new Produto(2 , "Jaqueta" , 150f);
+           Produto prod3 = new Produto(3 , "Mouse RGB" ,300f);
+           Produto prod4 = new Produto(1 , "PC Gamer" , 50000f);
+
+            Carrinho cart = new Carrinho();
+            cart.Cadastrar(prod1);
+            cart.Cadastrar(prod2);
+            cart.Cadastrar(prod3);
+            cart.Cadastrar(prod4);
+
+            cart.Deletar(prod1);
+
+            cart.Listar();
+
         }
     }
 }
